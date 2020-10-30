99b78d98be852c7c3c695f044f8a50de948d8286
Gontijoweb <gontijoweb@hotmail.com>
2020-10-30 13:30:44 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -34,3 +34,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
+
+//IOS verify
+
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  document.getElementById('install-ios').style.display = 'flex';
+}else{
+  document.getElementById('install-ios').style.display = 'none';
+}
