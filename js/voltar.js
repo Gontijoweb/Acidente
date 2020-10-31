51c5c21d653f5485b102ad52ff3eaaabbe936dda
Gontijoweb <gontijoweb@hotmail.com>
2020-10-31 11:55:18 -0300

    Delete voltar.js

--- js/voltar.js
+++
@@ -1,9 +0,0 @@
-var voltar = document.getElementById('voltar');
-
-voltar.addEventListener('click', goBack);
-
-function goBack(){
-  console.log('voltar');
-  document.getElementsByClassName('toggler').checked = false;
-  window.history.back();
-}
